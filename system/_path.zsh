e6bc2157e7a38a149b001623a94d430392ca7d89
Tej Chajed <tchajed@gmail.com>
2014-09-21 19:15:35 -0400

    Add system go binaries to PATH
    need this for godoc

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,6 @@
 # Depends on _env.sh variables.
 export PATH="./bin:$GOBIN:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# Go binaries (eg, godoc)
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
