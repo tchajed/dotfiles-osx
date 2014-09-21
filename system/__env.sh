37739b3180f974df3c65bd4dc3c1a359d28ab0d5
Tej Chajed <tchajed@gmail.com>
2014-09-21 10:20:11 -0400

    Add $GOBIN to environment.

---
+++ system/__env.sh
@@ -0,0 +1,5 @@
+# Needs to be executed first, used in _path.zsh
+export EDITOR='vim'
+export VISUAL='mvim'
+
+export GOBIN="$HOME/go/bin"
--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+# Depends on _env.sh variables.
+export PATH="./bin:$GOBIN:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-export PATH="$HOME/go/bin:$PATH"
