7b1e4e00cf37078f94843833b2c7a7fa87118d35
Tej Chajed <tchajed@gmail.com>
2014-10-04 10:54:32 -0400

    Make topdf not auto-complete existing PDFs

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# topdf converts to pdf, no point in completing existing PDFs
+compctl -g "^*.pdf" topdf
