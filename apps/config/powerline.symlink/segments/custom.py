d420de46633dd4850fd59d8282fb6fa20f7081bc
Tej Chajed <tchajed@gmail.com>
2014-11-06 08:42:20 -0500

    Hide battery indicator when battery is full

--- apps/config/powerline.symlink/segments/custom.py
+++ apps/config/powerline.symlink/segments/custom.py
@@ -1,4 +1,10 @@
 from powerline.segments import common
+from powerline.segments.common import bat
 
 def system_load(pl, num_avgs=3):
     return common.system_load(pl)[:num_avgs]
+
+def battery(pl, max_percent=101):
+    if bat._get_capacity(pl) < max_percent:
+        return common.battery(pl)
+    return []
