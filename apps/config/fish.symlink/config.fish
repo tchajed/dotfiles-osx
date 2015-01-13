725cef91b225b373e538947c84c231f0e295f270
Tej Chajed <tchajed@gmail.com>
2015-01-13 17:17:47 -0500

    Add some PATH directories

--- apps/config/fish.symlink/config.fish
+++ apps/config/fish.symlink/config.fish
@@ -12,5 +12,12 @@ source /usr/local/etc/autojump.fish
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
+# Homebrew sbin
+set PATH /usr/local/sbin $PATH
+# Go binaries
+set PATH ~/go/bin /usr/local/opt/go/libexec/bin $PATH
+# Scripts
+set PATH ~/scripts $PATH
+
 # Load oh-my-fish configuration.
 source $fish_path/oh-my-fish.fish
