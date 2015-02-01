d697234d8e0ba60d2e8f5dc35cbd63d789623406
Tej Chajed <tchajed@gmail.com>
2015-02-01 13:31:06 -0500

    Update autojump config line
    Homebrew changed the install location

--- apps/config/fish.symlink/config.fish
+++ apps/config/fish.symlink/config.fish
@@ -10,7 +10,7 @@ set fish_theme bobthefish
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # set fish_plugins autojump bundler
-source /usr/local/etc/autojump.fish
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
