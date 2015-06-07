88311e15b72d61084dc3f1a8522fd8b4e3dcc027
Tej Chajed <tchajed@gmail.com>
2015-06-07 13:18:42 -0400

    Add a new path dir for binaries
    e.g., a utility Go program, where the compiled binary shouldn't be
    committed so it can't go in .dotfiles/bin, so we symlink the binary from
    $GOPATH/bin/ to ~/bin.

--- apps/config/fish.symlink/config.fish
+++ apps/config/fish.symlink/config.fish
@@ -23,6 +23,8 @@ set PATH ~/go/bin /usr/local/opt/go/libexec/bin $PATH
 set PATH ~/scripts $PATH
 # Commited scripts in dotfiles
 set PATH ~/.dotfiles/bin $PATH
+# Uncommitted scripts and compiled binaries
+set PATH ~/bin $PATH
 # Haskell
 set PATH ~/.cabal/bin ~/Library/Haskell/bin $PATH
 
