1f09af5b3b785133dce83cd5a00fde7f69fdf410
Tej Chajed <tchajed@gmail.com>
2015-11-16 10:09:18 -0500

    Update powerline battery wrapper for new API

--- apps/config/powerline.symlink/segments/custom.py
+++ apps/config/powerline.symlink/segments/custom.py
@@ -4,6 +4,7 @@ def system_load(pl, num_avgs=3):
     return sys.system_load(pl)[:num_avgs]
 
 def battery(pl, max_percent=101):
-    if bat._get_capacity(pl) < max_percent:
+    capacity, ac_powered = bat._get_battery_status(pl)
+    if capacity < max_percent:
         return bat.battery(pl)
     return []
