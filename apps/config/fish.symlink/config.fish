bee83014c703b286b23dff5137fbc966270513d0
Tej Chajed <tchajed@gmail.com>
2016-04-24 09:20:45 -0400

    Confugure fish with iTerm2 integration

--- apps/config/fish.symlink/config.fish
+++ apps/config/fish.symlink/config.fish
@@ -101,3 +101,7 @@ function jhome
     set -Ux JAVA_HOME (/usr/libexec/java_home $argv)
     java -version
 end
+
+# iTerm 2 shell integration
+# https://iterm2.com/shell_integration.html
+source ~/.iterm2_shell_integration.fish
